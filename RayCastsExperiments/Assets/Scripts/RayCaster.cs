7d8bceb5a9d69c5499c77af6a61551b57f08b4c4
Damian Buczek <88405026+SZtamjan@users.noreply.github.com>
2023-06-11 02:55:32 +0200

    Test01
    Success, learned fully how rays and raycasts work and also learned how to check layer masks

--- RayCastsExperiments/Assets/Scripts/RayCaster.cs
+++ RayCastsExperiments/Assets/Scripts/RayCaster.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class RayCaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera cam;
+    public GameObject debugerO;
+    public GameObject debugerT;
+    public float raycastDistance;
+    public LayerMask lejer;
+    Vector3 forw = Vector3.forward;
+    private void Update()
     {
-        
-    }
+        Vector3 cursorPos = Input.mousePosition;
+        cursorPos.z += 10f;
+        // debugerO.transform.position = cursorPos;
+        // Vector3 target = cam.ScreenToWorldPoint(cursorPos);
+        // debugerT.transform.position = target;
+        // Ray ray = new Ray(cam.transform.position,target);
+        // Debug.DrawRay(cam.transform.position,target);
 
-    // Update is called once per frame
-    void Update()
-    {
+        print(lejer.value);
+        Ray ray = cam.ScreenPointToRay(cursorPos);
         
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            
+            if (((1<<hit.collider.gameObject.layer) & lejer) != 0)
+            {
+                Debug.Log("Trafiono obiekt: " + hit.collider.gameObject.name);
+            }
+        }
+        //Debug.Log(hit.collider.gameObject.layer);
+        Debug.DrawRay(ray.origin,ray.direction);
     }
 }
